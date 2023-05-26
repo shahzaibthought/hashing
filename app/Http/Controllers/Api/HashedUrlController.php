f933c53baef5e91a96b6cdb312855cb9e7662cc8
Shahzaib Khan <shahzaib.thought@gmail.com>
2023-05-26 19:33:11 +0530

    Created and injected the request class to validate the incoming data.

--- app/Http/Controllers/Api/HashedUrlController.php
+++ app/Http/Controllers/Api/HashedUrlController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\HashedUrl\StoreRequest;
 
 class HashedUrlController extends Controller
 {
     /**
+     * @param StoreRequest $request
+     *
      * @return string
      */
-    public function store()
+    public function store(StoreRequest $request)
     {
         return 'Welcome to HashedUrlController ... Good bye!';
     }
