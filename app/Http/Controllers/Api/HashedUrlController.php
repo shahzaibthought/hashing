61765bebd2ac8e2c1f0ecea7b162f744dc6e9313
Shahzaib Khan <shahzaib.thought@gmail.com>
2023-05-26 19:31:07 +0530

    Created an API route and corrosponding controller to generate the hashed URL.

---
+++ app/Http/Controllers/Api/HashedUrlController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+class HashedUrlController extends Controller
+{
+    /**
+     * @return string
+     */
+    public function store()
+    {
+        return 'Welcome to HashedUrlController ... Good bye!';
+    }
+}
